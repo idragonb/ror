3244411b236ad89c785bd97c2481118b895a8de3
Michael Berman <git@ilact.com>
2013-07-21 12:42:42 +0300

    First try using github windows

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '3ef8a1cefa816db9fa538f418913414da6f3bc00806486bbd5cc647a699d7343ef5072996f6b39af3ca4469651ced77b516615bbaa21440534f013d1d9ebdf42'
